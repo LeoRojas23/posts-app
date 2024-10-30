a7de67569bb319af461822ab18d5f21a82922659
Leo Rojas <leorojas8802@gmail.com>
2024-10-30 14:49:07 -0300

    refactor time and date functions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -29,87 +29,45 @@ export const regexNoWhitespaceOnly = /^(?!\s*$).+/
 
 export const regexComment = /^(?=.*\S)(?!\s+$)(.|\n){0,150}$/
 
-export function timeAgoPost(timestamp: string) {
-  const now = new Date().getTime()
+const timeUnits = [
+  { unit: 'year', seconds: 31536000 },
+  { unit: 'month', seconds: 2592000 },
+  { unit: 'day', seconds: 86400 },
+  { unit: 'hr', seconds: 3600 },
+  { unit: 'min', seconds: 60 },
+  { unit: 'sec', seconds: 1 },
+]
+
+export function timeAgo(timestamp: string) {
+  const now = Date.now()
   const diff = (now - new Date(timestamp).getTime()) / 1000
 
   if (diff < 1) {
     return 'now'
   }
 
-  const minutes = Math.floor(diff / 60)
-  const hours = Math.floor(minutes / 60)
-  const days = Math.floor(hours / 24)
-  const months = Math.floor(days / 30)
-  const years = Math.floor(months / 12)
+  for (const { unit, seconds } of timeUnits) {
+    const interval = Math.floor(diff / seconds)
 
-  if (years > 0) {
-    return `${years} year`
-  } else if (months > 0) {
-    return `${months} month`
-  } else if (days > 0) {
-    return `${days} day`
-  } else if (hours > 0) {
-    return `${hours} hr`
-  } else if (minutes > 0) {
-    return `${minutes} min`
-  } else {
-    return `${Math.floor(diff)} sec`
+    if (interval > 0) {
+      return `${interval} ${unit}${interval > 1 ? 's' : ''}`
+    }
   }
-}
-
-export function timeAgoComment(timestamp: string) {
-  const now = new Date().getTime()
-  const diff = (now - new Date(timestamp).getTime()) / 1000
-
-  if (diff < 1) {
-    return 'now'
-  }
-
-  const minutes = Math.floor(diff / 60)
-  const hours = Math.floor(minutes / 60)
-  const days = Math.floor(hours / 24)
-
-  if (days > 0) {
-    const dateFormatter = new Intl.DateTimeFormat('en-US', {
-      weekday: 'short',
-      month: 'short',
-      day: 'numeric',
-    })
-
-    return dateFormatter.format(new Date(timestamp))
-  }
-
-  if (hours > 0) {
-    return `${hours}h`
-  }
-
-  if (minutes > 0) {
-    return `${minutes}m`
-  }
-
-  return `${Math.floor(diff)}s`
 }
 
 export function formatDatePostId(dateString: string) {
   const date = new Date(dateString)
 
   const dateFormatter = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
     hour: 'numeric',
     minute: 'numeric',
     hour12: true,
-    month: 'short',
-    day: 'numeric',
-    year: 'numeric',
   })
 
   const formattedDate = dateFormatter.format(date)
 
-  const dateParts = formattedDate.split(', ')
-
-  const time = dateParts.slice(0, 2).join(', ')
-
-  const datePart = dateParts.slice(2).join('')
-
-  return `${datePart} · ${time}`
+  return formattedDate.replace(',', ' ·')
 }
