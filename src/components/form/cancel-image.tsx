d1d2f9bd750e8cc28450a3fb9bb132e2537ce39b
Leo Rojas <leorojas8802@gmail.com>
2025-06-06 15:01:53 -0300

    add CancelImage component for clearing image data

---
+++ src/components/form/cancel-image.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useActionState } from 'react'
+
+import Icon from '../icon'
+import FormButton from '../button/form-button'
+
+import { clearImageData } from '@/actions/actions'
+
+interface Props {
+  onClick: () => void
+}
+
+export default function CancelImage({ onClick }: Props) {
+  const [, formAction, formActionPending] = useActionState(async () => {
+    onClick()
+
+    return await clearImageData()
+  }, null)
+
+  return (
+    <FormButton
+      className='absolute top-1 left-1 cursor-pointer rounded-full bg-[#a50f0f] p-1 transition-opacity duration-100 ease-linear hover:opacity-80 sm:p-1.5'
+      formAction={formAction}
+      pending={formActionPending}
+      type='submit'
+    >
+      <Icon className='h-5 w-5 text-[#fff1f1]' id='close' />
+    </FormButton>
+  )
+}
